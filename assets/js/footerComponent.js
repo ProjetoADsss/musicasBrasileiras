8df276055966877f1fc21b1e01f2012eb925e4a3
adailton.santos <adailtonaparecido69@gmail.com>
2024-11-11 11:44:15 -0300

    Feat: Adiciona rodapé e adiciona data do projeto

---
+++ assets/js/footerComponent.js
@@ -0,0 +1,10 @@
+function footer() {
+    return `
+    <div>
+        <img src="./assets/images/logo-letreiro.png" alt="logo-projeto" class="logo-projeto">
+    </div>
+    <p>2024</p>
+    `
+}
+
+module.exports = footer
